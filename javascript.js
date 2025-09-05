7d0a43dc4f4ce338eff58af4cede09f0ce884173
anandb104 <210169445+anandb104@users.noreply.github.com>
2025-09-05 13:06:25 +0530

    Add function to change the number of rows and columns in grid
    A Button was placed to change the number of rows and columns in the grid and also hover was applied so that on hovering the grid leaves a trail of color.

--- javascript.js
+++ javascript.js
@@ -2,9 +2,34 @@ const container=document.querySelector("#container");
 let div=[];
 for(i=0;i<256;i++){
 div[i]=document.createElement("div");
+div[i].style.width="60px";
+div[i].style.height="60px";
 container.appendChild(div[i]);
 }
 for(let i=0;i<256;i++){
 div[i].addEventListener("mouseover",()=>{div[i].classList.add("new");})
 }
+const btn=document.querySelector("button");
+btn.addEventListener("click",()=>{
+    let input=prompt("Enter the number of rows",64)
+    // for(i=0;i<256;i++){
+    //     container.removeChild(div[i]);
+    //     }
+    let divi=[];
+    divi=container.querySelectorAll("div");
+    const length=divi.length;
+    for(i=0;i<length;i++){
+    container.removeChild(div[i]);
+    }
+    for(let i=0;i<((Number(input))*(Number(input)));i++){
+        div[i]=document.createElement("div");
+        div[i].style.width=`${960/Number(input)}px`;
+        div[i].style.height=`${960/Number(input)}px`;
+        container.appendChild(div[i]);
+    }
+    for(let i=0;i<((Number(input))*(Number(input)));i++){
+        div[i].addEventListener("mouseover",()=>{div[i].classList.add("new");})
+        }
+});
+
 
