b078fb4913e93e71bed95b5fe7a26e33b1df5730
anandb104 <210169445+anandb104@users.noreply.github.com>
2025-09-20 15:30:55 +0530

    Add opacity change for black and violet colors and bug fix
    Opacity change feature was added such that it changes with 0.1 on each hover and a bug fixed which caused black color class to be added on first hover of button click on each div.

--- javascript.js
+++ javascript.js
@@ -5,6 +5,7 @@ div[i]=document.createElement("div");
 div[i].style.width="60px";
 div[i].style.height="60px";
 container.appendChild(div[i]);
+
 }
 const btn1=document.querySelector("#row-setter");
 const btn2=document.querySelector("#rgb-setter");
@@ -15,6 +16,7 @@ const btn6=document.querySelector("#random-setter");
 for(let i=0;i<256;i++){
 div[i].onmouseover = () => div[i].classList.add("black-color");
 }
+
 
 btn1.addEventListener("click",()=>{
     let input=prompt("Enter the number of rows",64)
@@ -75,6 +77,8 @@ btn1.addEventListener("click",()=>{
 
 let mode = null;
 let j=1;
+let k=1;
+let p=1;
 container.addEventListener("mouseover", (e) => {
 
   if (e.target.tagName === "DIV") {
@@ -82,21 +86,27 @@ container.addEventListener("mouseover", (e) => {
       
         e.target.classList.remove("black-color");
       e.target.classList.add("rgb-color");
+      e.target.style.opacity=`${k/10}`; 
+      k++;
+      if(k==11)k=1;
     } else if (mode === "black") {
 
         e.target.classList.remove("rgb-color");
       e.target.classList.add("black-color");
-      
+      e.target.style.opacity=`${p/10}`; 
+      p++;
+      if(p==11)p=1; 
     }
     else if (mode === "random") {
         let x=Math.random();
         let y=Math.random();
         let z=Math.random();
-            e.target.onmouseover = ()=>{e.target.style.backgroundColor=`rgb(${x*100},${y*100},${z*100})`;
-
+            e.target.style.backgroundColor=`rgb(${x*100},${y*100},${z*100})`;
+            e.target.classList.remove("black-color");
+            e.target.classList.remove("rgb-color");
             e.target.style.opacity=`${j/10}`; 
             j++;
-            if(j==11)j=1;};
+            if(j==11)j=1;
         }
 }
 });
@@ -104,13 +114,5 @@ container.addEventListener("mouseover", (e) => {
 
 btn2.addEventListener("click", () => mode = "rgb");
 btn3.addEventListener("click", () => mode = "black");
-btn6.addEventListener("click", () => {
-  let divi=[];
-  divi=container.querySelectorAll("div");
-  const length=divi.length;
-  for(i=0;i<length;i++){
-  div[i].classList.remove("rgb-color");
-  div[i].classList.remove("black-color");
-  }
-  mode = "random"});
+btn6.addEventListener("click", () => mode = "random");
 
